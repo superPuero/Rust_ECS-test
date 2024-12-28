d3b8ea8300c053dade9abe601f151bafda0ce693
unknown <ar73mch1k@gmai.com>
2024-12-28 15:13:13 +0100

    made code more "rusty"

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::any::Any;
 struct Registry{
     counter: u32,
     entities: HashSet<u32>,
-    component: HashMap<u32, HashMap<TypeId, Box<dyn Any>>>
+    component: HashMap<(u32, TypeId), Box<dyn Any>>
 }
 
 impl Registry{
@@ -33,24 +33,20 @@ impl Registry{
     }
 
     fn add_component<C: Any>(&mut self, e_id: u32, comp: C){
-        let c_key = TypeId::of::<C>();
+        let c_id = TypeId::of::<C>();
         self.component
-                    .entry(e_id)
-                    .or_insert(HashMap::new())
-                    .entry(c_key)
+                    .entry((e_id, c_id))
                     .or_insert(Box::new(comp));
 
         println!("Added component {:#?}, to entity {}", type_name::<C>(), e_id);
     }
 
-    fn get_component<C: Any>(&mut self, e_id: u32) -> &mut C{
-        self.component
-                    .get_mut(&e_id)
-                    .unwrap()
-                    .get_mut(&TypeId::of::<C>())
-                    .unwrap()
-                    .downcast_mut::<C>()
-                    .unwrap()
+    fn get_component<C: Any>(&mut self, e_id: u32) -> Result<&mut C, String>{
+        let key = &(e_id, TypeId::of::<C>());
+        match self.component.get_mut(key){
+            Some(val) => Ok(val.downcast_mut::<C>().unwrap()),
+            None => Err(format!("No such element"))
+        }     
     }
 
 }
@@ -68,24 +64,19 @@ fn main() {
     let id1 = r.add_entity();
     let id2 = r.add_entity();
 
-
     r.add_component(id1, Transform{x: 12, y: 43});
-    r.add_component(id1, Foo{name: String::from("Fooer")});
-
+    r.add_component(id1, Foo{name: "Fooer".into()});
 
     r.add_component(id2, Transform{x: 543, y: 345});
-    r.add_component(id2, Foo{name: String::from("Boo")});
+    r.add_component(id2, Foo{name: "boo".into()});
 
-
-    let t1 = r.get_component::<Transform>(id1);
+    let t1 = r.get_component::<Transform>(id1).unwrap();
     println!("x: {}, y: {}", t1.x, t1.y);
     
-    println!("name: {}", r.get_component::<Foo>(id1).name);
-
+    println!("name: {}", r.get_component::<Foo>(id1).unwrap().name);
     
-    let t2 = r.get_component::<Transform>(id2);
+    let t2 = r.get_component::<Transform>(id2).unwrap();
     println!("x: {}, y: {}", t2.x, t2.y);
     
-    println!("name: {}", r.get_component::<Foo>(id2).name);
-
+    println!("name: {}", r.get_component::<Foo>(id2).unwrap().name);
 }
